87b6b5bd9e3cba02e8cebc8cf002695bef14b484
Dancanoduor <dancanoduor1990@gmail>
2019-12-01 12:16:43 +0300

    Add templates & configuations

--- config.py
+++ config.py
@@ -1,27 +1,36 @@
+Learn more or give us feedback
+import os
+
 class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/bloglist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-
-
+    
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+    
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/bloglist_test'
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
+        
+        
+        
+config_options = { 
+    'development' : DevConfig,
+    'production' : ProdConfig,
+    'test':TestConfig
+}
