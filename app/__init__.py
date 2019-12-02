139a7c7d1707e5211bcc8308a2c89d66aa66a1f3
Dancanoduor <dancanoduor1990@gmail>
2019-12-02 09:43:06 +0300

    Add files

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_moment import Moment
+# from flask_comment import Comment
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -16,7 +16,7 @@ db = SQLAlchemy()
 def create_app(config_name):
     
     app = Flask(__name__)
-    moment = Moment(app)
+    # comment = Comment(app)
     
     #Creating app configurations
     
--- app/blogs/forms.py
+++ app/blogs/forms.py
@@ -1,6 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField,SubmitField,TextAreaField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email,EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField( 'Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField( 'Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
 class PostForm(FlaskForm):
     title = StringField('Title',validators=[DataRequired()])
